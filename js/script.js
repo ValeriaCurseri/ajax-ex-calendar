d255840f67a02bfbc8d565c9d9d77cb367579460
Valeria Curseri <curserivaleria@gmail.com>
2020-08-31 14:21:50 +0200

    File di base

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Link API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
+
+// Creiamo un calendario dinamico con le festività.
+// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+
+// Milestone 1
+// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+
+// Milestone 2
+// Diamo la possibilità di cambiare mese, gestendo il caso in cui l’API non possa ritornare festività.
+// Attenzione!
+// Ogni volta che cambio mese dovrò:
+// Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018)
+// Controllare quanti giorni ha il mese scelto formando così una lista
+// Chiedere all’API quali sono le festività per il mese scelto
+// Evidenziare le festività nella lista
